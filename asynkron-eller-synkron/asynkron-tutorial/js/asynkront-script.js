7f6580193e834cde4979c2670d2e6394ca8bc6d7
Ola Karlsson <ola@olakarlsson.com>
2017-02-10 17:47:28 +0100

    Uppdateringar av koden och lade in kommentarer

--- asynkron-eller-synkron/asynkron-tutorial/js/asynkront-script.js
+++ asynkron-eller-synkron/asynkron-tutorial/js/asynkront-script.js
@@ -1,22 +1,36 @@
-//Anropa functionen som ber om användarens namn genom en prompt på sidan
+/*/////////////////////////////////////////////////////////
+
+Demonstration av asynkron kod med Promise av Ola Karlsson
+http://www.olakarlsson.com, @o_k
+
+/////////////////////////////////////////////////////////*/
+
+
+
+//Anropa functionen som hämtar till exempel ett användarnamn
 //använd sedan .then för att "vänta" tills resultatet kommer tillbaka
 getName().then(function(name){
-    //Skriv ut namnet använadaren skrev in efter det kommit tillbaka 
+    //Skriv ut namnet efter det kommit tillbaka från funktionen som anropades 
     console.log(name);
 })
 
 
-console.log("Kod som ligger efter anropet till funktionen för att hämta användarens namn");
+
+/*Trots att denna kod ligger efter anropet till getName och koden som ska skriva ut namnet, kommer detta att skrivas ut först eftersom efter anropet till getName() kommer koden fortsätta, och inte blockaras, trots att resultatet från funktionsanropen inte kommit tillbaka ännu.
+Det är detta som menas med att koden är Asynkron */
+console.log("Kod som ligger efter anropet till funktionen för att hämta namnet");
 
 
-
+//Här är en funktion som har en fördröjning för att låtsas att man gör ett anrop till en webbtjänst 
+//eller en databas för att hämta användarens namn och att det tar lite tid
 function getName(){
-    
+    //Här skickas tillbaka ett Promise (löfte) om att där kommer komma ett svar
     return new Promise(function(resolve, reject) {
 
+        //Skapa en timer som väntar 2 sekunder
        setTimeout(function(){
-        resolve("Jan Svensson")}, 2000);
-        
+        //Efter 2 sekunder(2000 millisekunder) körs denna kod
+        resolve("Jan Svensson")
+       },2000); 
     });
-    
 }
--- asynkron-eller-synkron/asynkron-tutorial/js/synkront-script.js
+++ asynkron-eller-synkron/asynkron-tutorial/js/synkront-script.js
@@ -1,13 +1,31 @@
+/*/////////////////////////////////////////////////////////
+
+Demonstration av synkron kod med Promise av Ola Karlsson
+http://www.olakarlsson.com, @o_k
+
+/////////////////////////////////////////////////////////*/
+
+
+//I synkron programmering utförs allt i ordning och gör man ett anrop till en 
+//funtion någon annanstans i koden, så väntas det inte på att svaret ska komma tillbaka utan saker går vidare ändå.
+
+
+//Gör ett anrop till getName funktionen och sätt in svaret i variabeln name
 var name = getName();
+
+//Skriv ut värdet i variabeln name
 console.log(name);
 
-console.log("Kod som kommer efter att anropet till getUserName har hänt");
+//Skriv sedan ut något annat, lägg märke till att saker händer i den ordning de ligger i koden
+console.log("Kod som kommer efter att anropet till getName");
 
-
-
+//Här är en funktion som har en fördröjning för att låtsas att man gör ett anrop till en webbtjänst 
+//eller en databas för att hämta användarens namn och att det tar lite tid
 function getName(){
     
     setTimeout(function(){
-        return "Jan Svensson"}, 1000);
+        //Efter en 2 sekunder (2000 millisekunder)
+        return "Jan Svensson"
+    }, 2000);
     
 }
